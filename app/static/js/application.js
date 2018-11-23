81711f296d515c3de5ecf7122fb7c25ca0bc4812
Annie Wong <anniew168@gmail.com>
2018-11-23 00:40:40 -0800

    uh

--- app/static/js/application.js
+++ app/static/js/application.js
@@ -35,5 +35,3 @@ $("#input-form").on("submit", function(event) {
   outbox.send(JSON.stringify({ handle: handle, text: text }));
   $("#input-text")[0].value = "";
 });
-
-server.listen(process.env.PORT || port)
